47ccce673b07821fee2c11709537b978ebd8a6ac
ceneax <50333259+ceneax@users.noreply.github.com>
2021-01-11 15:26:19 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    val kotlinVersion = "1.4.21"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+
+    id("net.mamoe.mirai-console") version "2.0-M2"
+}
+
+group = "org.example"
+version = "0.1.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    jcenter()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+}
